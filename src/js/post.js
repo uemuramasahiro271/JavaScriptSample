005e226ca6e5489784c464a2e6c594dc73b87826
uemuramasahiro271 <uemura.free2020@gmail.com>
2020-06-10 00:51:38 +0900

    実装

--- src/js/post.js
+++ src/js/post.js
@@ -17,15 +17,26 @@ class PostItemCreator {
         // $(`#${id} div`).append(`内容：\n${this.text}`);
         // $(`#${id}`).append(`<div class="border"></div>`);
 
+        var count = `${postCount}`;
         var id = `content_${postCount}`;
         var date = new Date();
         var contributor = this.contributor;
         var text = this.text;
         $("#contents_area").append(`<article id="${id}"></article>`);
         $(`#${id}`).load("./parts/postItem.html", function() {
-            $(`#${id} .contributor`).text(`投稿者：${contributor}`);
-            $(`#${id} .date`).text(date.toLocaleString("ja"));
-            $(`#${id} .text`).text(`内容：\n${text}`);
+            // 各IDを設定
+            $('#v_post_no').attr('id', `post_no${count}`);
+            $('#v_date').attr('id', `data${count}`);
+            $('#v_contributor').attr('id', `contributor${count}`);
+            $('#v_text').attr('id', `text${count}`);
+            $('#v_edit_btn').attr('id', `edit_btn${count}`);
+            $('#v_delete_btn').attr('id', `delete_btn${count}`);
+
+            // 投稿内容設定
+            $(`#post_no${count}`).text(`No：${count}`);
+            $(`#data${count}`).text(date.toLocaleString("ja"));
+            $(`#contributor${count}`).text(`${contributor}`);
+            $(`#text${count}`).text(`${text}`);
         });
 
         //$(`#contributor_aa`).text("aa");
